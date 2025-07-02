44ff98981ff5b922ce08108df62035cf718935b8
vfa-khuongdv <khuongdv@vitalify.asia>
2025-07-02 19:40:26 +0700

    fix: change sha

--- Formula/delphi-csharp.rb
+++ Formula/delphi-csharp.rb
@@ -2,7 +2,7 @@ class DelphiCsharp < Formula
   desc "A CLI tool to convert Delphi code to C#"
   homepage "https://github.com/vfa-khuongdv/homebrew-delphi-csharp"
   url "https://github.com/vfa-khuongdv/homebrew-delphi-csharp/releases/download/v1.0.0/delphi-csharp-1.0.0.tar.gz"
-  sha256 "85c5800626f8b1a2361964d8c72f6588929b61080b419a81c8d58d136b5c502b"
+  sha256 "90083d2bf7774bf8127bca8283879a3a14f2dc8d56d51be569c61b2f588f8645"
   license "MIT"
 
   depends_on "node"
