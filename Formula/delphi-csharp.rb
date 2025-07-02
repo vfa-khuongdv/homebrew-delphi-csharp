34c480952b9d6a5fa10d8c9716df11d74a061383
vfa-khuongdv <khuongdv@vitalify.asia>
2025-07-02 19:31:38 +0700

    fix: remove folder build

--- Formula/delphi-csharp.rb
+++ Formula/delphi-csharp.rb
@@ -2,7 +2,7 @@ class DelphiCsharp < Formula
   desc "A CLI tool to convert Delphi code to C#"
   homepage "https://github.com/vfa-khuongdv/homebrew-delphi-csharp"
   url "https://github.com/vfa-khuongdv/homebrew-delphi-csharp/releases/download/v1.0.0/delphi-csharp-1.0.0.tar.gz"
-  sha256 "6ebc93bb631ef72a3201f67b7680cb016b990b640f1f0b25d5a8eee85e99a73d"
+  sha256 "a4274def4feae4561698561cbdd6e3a502254500ed35a2491bc2abb0fa51850d"
   license "MIT"
 
   depends_on "node"
